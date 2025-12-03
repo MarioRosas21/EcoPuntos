86bd2935b17cc7795f31f7b24179ae0b501336da
Mario Ricardo Rosas Benitez <Mariorosas7711@gmail.com>
2025-12-03 15:59:50 -0600

    rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,22 +8,68 @@ import Rewards from "./pages/Rewards";
 import History from "./pages/History";
 import Profile from "./pages/Profile";
 import Know from "./pages/know";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import "./styles.css";
+
 export default function App() {
   return (
     <Router>
-      {" "}
       <Routes>
-        {" "}
-        <Route path="/" element={<Login />} />{" "}
-        <Route path="/register" element={<Register />} />{" "}
-        <Route path="/home" element={<Home />} />{" "}
-        <Route path="/scan" element={<Scan />} />{" "}
-        <Route path="/rewards" element={<Rewards />} />{" "}
-        <Route path="/history" element={<History />} />{" "}
-        <Route path="/profile" element={<Profile />} />{" "}
-        <Route path="/know" element={<Know />} />{" "}
-      </Routes>{" "}
+        {/* Rutas públicas */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Rutas protegidas */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/scan"
+          element={
+            <ProtectedRoute>
+              <Scan />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/rewards"
+          element={
+            <ProtectedRoute>
+              <Rewards />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <History />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/know"
+          element={
+            <ProtectedRoute>
+              <Know />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
